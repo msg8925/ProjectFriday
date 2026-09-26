d9ced1eaae2b8f2f515489cb360602d72e8c9c6c
msg8925 <msg.8925@gmail.com>
2026-09-26 16:08:35 +0700

    Update appsScriptUrl and sharedKey in config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ window.TEST_CONFIG = {
 
   // Google Apps Script web-app URL (README, step 2). While this is not set, the page
   // marks locally using teacher-only/answer-key.js, so you can preview it on your own computer.
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyypbnWj2xiElHgJzYH3m36wCq9g15VoEusQmUIGI9SXcPL9uUN8R6h2cvSDyZ0nGgR_Q/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycby8NcN2c-LCUDNDWrQfo2DYx_bIImEa1ew90D-3MEnuFp8c2vjB2DQcj66bzjpwO-G_ww/exec',
   // Must match SHARED_KEY in Code.gs. Stops random people posting to your script.
-  sharedKey: 'U4j3@Fed7*#hbh33bSc/d?4fmsk$1S',
+  sharedKey: '7Dh6#8&JggyHW@-Id%mNi8*$gGqJ^h',
 
   // Show a "Start code" box. Set the code itself in Code.gs (START_CODE) so students can't read it here.
   askStartCode: true,
